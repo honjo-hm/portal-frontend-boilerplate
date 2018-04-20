3a7e3fff41dbaea2fe1e811ded4a687c57573908
honjo hiroki <isthhonj@mediba.jp>
2018-04-20 18:07:58 +0900

    wip

---
+++ app/next.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const withTypescript = require('@zeit/next-typescript')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+module.exports = withTypescript({
+    webpack(config, options) {
+        if (options.isServer) {
+            config.plugins.push(new ForkTsCheckerWebpackPlugin({
+                tslint: true,
+                tsconfig: path.resolve(config.context, './tsconfig.json')
+            }))
+            // config.node = {
+            //     fs: "empty"
+            // }
+        }
+        // config.node = {
+        //     fs: "empty"
+        // }
+        config.node = {
+            fs:     "empty",
+            module: "empty",
+            net:    "empty",
+            tls:    "empty"
+        }
+        return config
+    },
+})

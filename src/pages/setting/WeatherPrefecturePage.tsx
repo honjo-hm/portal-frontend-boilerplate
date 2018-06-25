c2419333d9363af03260b58d33cd957cea60eef4
根岸 遼太郎 <negishi@mediba.jp>
2018-06-25 16:58:51 +0900

    xxxx :+1: Pageのスタイルを外部ファイル

--- src/pages/setting/WeatherPrefecturePage.tsx
+++ src/pages/setting/WeatherPrefecturePage.tsx
@@ -1,65 +1,13 @@
 /**
- * Setting Weather Region Page
+ * 天気設定(都道府県選択)
  *
  * @package Page
  * @since 2018.XX.XX
  * @copyright mediba.inc
  */
+import {} from "../../shared/style/setting/global";
 
 import SettingPrefectureTemplate from "../../shared/components/templates/SettingPrefectureTemplate";
-import { injectGlobal } from "styled-components";
-
-/**
- * style
- */
-injectGlobal`
-    html, body, div, span, nav, ul, li, h1, h2, p, a {
-        margin: 0;
-        padding: 0;
-        border: 0;
-        outline: 0;
-        font-weight: inherit;
-        font-style: inherit;
-        font-family: inherit;
-        font-size: 100%;
-        vertical-align: baseline;
-    }
-
-    html {
-        height: 100%;
-        display: block;
-        vertical-align: baseline;
-    }
-
-    body {
-        line-height: 1;
-        background: #fff;
-        font-size: 14px;
-        font-family: sans-serif;
-        color: #3c3c3c;
-        display: block;
-    }
-
-    ul {
-        list-style: none;
-    }
-
-    section {
-        margin: 0;
-        padding: 0;
-        border: 0;
-        outline: 0;
-        display: block;
-    }
-
-    a {
-        text-decoration: none;
-    }
-
-    a, a:active, a:hover, a:link {
-        color: #3c3c3c;
-    }
-`;
 
 // module
 export default () => (
